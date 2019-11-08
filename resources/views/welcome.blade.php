0d7102ef2a144791c5455c361669017147436016
Daniel Andres Zarate <daniel.andres.zarate@gmail.com>
2019-11-08 11:44:46 -0500

    Commit 8 noviembre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Daniel Zarate.
                 </div>
 
                 <div class="links">
